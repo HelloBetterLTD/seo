0b5fd53d5218f569a276f5631595a9b773f9ab13
Nivanka Fonseka <nivanka@silverstripers.com>
2017-12-05 16:44:12 +0530

    first JS editor model

--- client/src/components/SEOEditorHolder/SEOEditorHolder.js
+++ client/src/components/SEOEditorHolder/SEOEditorHolder.js
@@ -12,14 +12,36 @@ class SEOEditorHolder extends React.Component {
 
     render() {
         return(<div className="seo-editor">
-            <ul>
-                <li><a href='#'>SEO</a></li>
-                <li><a href='#'>Social</a></li>
-                <li><a href='#'>Settings</a></li>
-            </ul>
-            </div>);
+            <nav>
+                <ul>
+                    <li><a href='#'><i className="icon font-icon-rocket"></i></a></li>
+                    <li><a href='#'><i className="icon font-icon-globe-1"></i></a></li>
+                    <li><a href='#'><i className="icon font-icon-cog"></i></a></li>
+                </ul>
+            </nav>
+            <div className="seo-tab-container">
+                <div className="seo-tab">
+                    <h4>SEO Data</h4>
+                    <div class='seo-input'>
+                        <input type='text' name='name[MetaTitle]' />
+                    </div>
+                </div>
+                <div className="seo-tab">
+                    <h4>Social Data</h4>
+                    <div class='seo-input'>
+                        <input type='text' name='name[MetaTitle]' />
+                    </div>
+                </div>
+                <div className="seo-tab">
+                    <h4>Settings</h4>
+                    <div class='seo-input'>
+                        <input type='text' name='name[MetaTitle]' />
+                    </div>
+                </div>
+            </div>
+        </div>);
     }
 
 }
 
-export default { SEOEditorHolder };
+export default SEOEditorHolder;
