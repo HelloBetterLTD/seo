27ae3ed371a486cefbf7bf74abba07a625de580c
Bim <bim.legaspi@cubro.co.nz>
2023-03-13 10:57:17 +0800

    use override object when used

--- src/Extension/SEODataExtension.php
+++ src/Extension/SEODataExtension.php
@@ -756,7 +756,7 @@ class SEODataExtension extends DataExtension
 
     public function ComputeMetaTitle()
     {
-        $record = $this->owner;
+        $record = SEODataExtension::get_override() ? : $this->owner;
         $metaTitle = $record->obj('MetaTitle')->forTemplate();
         if (!$metaTitle) {
             $metaTitle = $record->obj('Title')->forTemplate();
@@ -770,7 +770,7 @@ class SEODataExtension extends DataExtension
 
     public function ComputeMetaDescription()
     {
-        $record = $this->owner;
+        $record = SEODataExtension::get_override() ? : $this->owner;
         $metaDescription = $record->obj('MetaDescription')->getValue();
         if (!$metaDescription && ($fallbackField = $record->config()->get('fallback_meta_description')) && $record->obj($fallbackField)) {
             $metaDescription = $record->dbObject($fallbackField)->forTemplate();
